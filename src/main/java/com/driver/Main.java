aa9c1878741d89388b2e23bd3729d264786e9e53
unknown <mjrajak47z@gmail.com>
2023-03-16 02:18:08 +0530

    Overriding Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args){
+
+        B obj1 = new B();
+        String ans1 = obj1.meth();
+        B obj2 = new B();
+        String ans2 = obj2.meth();
+
+        System.out.println(ans1);
+        System.out.println(ans2);
+    }
 }
